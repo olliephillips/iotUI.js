855642217ea6a55ac6f6554f3f9c002f5c0e63c7
Ollie Phillips <oliver@eantics.co.uk>
2016-11-08 17:24:36 +0000

    Added compiled tag to iotUI.js

--- iotUI.js
+++ iotUI.js
@@ -336,6 +336,170 @@ riot.tag2('iotui-controller', '<div riot-style="width:{opts.width}px;height:{opt
 	})
 }, '{ }');
 
+// F1Wheel
+riot.tag2('iotui-f1wheel', '<div riot-style="height:{opts.height}px;width:{opts.width}px;margin-left:auto;margin-right:auto;"> <svg id="riot-f1wheel" style="width:100%;height:100%;" viewbox="0 0 523 341" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <defs> <radialgradient cx="119.095644%" cy="52.0301464%" fx="119.095644%" fy="52.0301464%" r="14.6438989%" id="radialGradient-1"> <stop stop-color="#494949" offset="0%"></stop> <stop stop-color="#161616" offset="100%"></stop> </radialGradient> <lineargradient x1="100%" y1="42%" x2="59.0642105%" y2="42%" id="linearGradient-2"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <lineargradient x1="-42.9665647%" y1="101.754319%" x2="-42.9665476%" y2="19.8827403%" id="linearGradient-3"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <lineargradient x1="50.1105719%" y1="99.7080827%" x2="50.110596%" y2="-0.584348656%" id="linearGradient-4"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <lineargradient x1="91.5044266%" y1="30.4937341%" x2="16.0759438%" y2="17.4454618%" id="linearGradient-5"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <lineargradient x1="96.207177%" y1="84.4571859%" x2="34.4929582%" y2="97.2764936%" id="linearGradient-6"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <radialgradient cx="-19.0810003%" cy="52.0301464%" fx="-19.0810003%" fy="52.0301464%" r="14.6438989%" id="radialGradient-7"> <stop stop-color="#494949" offset="0%"></stop> <stop stop-color="#161616" offset="100%"></stop> </radialGradient> <lineargradient x1="0%" y1="42%" x2="40.9357895%" y2="42%" id="linearGradient-8"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <lineargradient x1="142.967035%" y1="101.754319%" x2="142.967018%" y2="19.8827403%" id="linearGradient-9"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <lineargradient x1="49.8892495%" y1="99.7080827%" x2="49.8892254%" y2="-0.584348656%" id="linearGradient-10"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <lineargradient x1="8.49543826%" y1="30.4937341%" x2="83.923921%" y2="17.4454618%" id="linearGradient-11"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <lineargradient x1="3.79268783%" y1="84.4571859%" x2="65.5069066%" y2="97.2764936%" id="linearGradient-12"> <stop stop-color="#191919" offset="0%"></stop> <stop stop-color="#4C4C4C" offset="48%"></stop> <stop stop-color="#191919" offset="96%"></stop> </linearGradient> <radialgradient cx="-19.0810003%" cy="52.0329748%" fx="-19.0810003%" fy="52.0329748%" r="14.6438989%" id="radialGradient-13"> <stop stop-color="#494949" offset="0%"></stop> <stop stop-color="#161616" offset="100%"></stop> </radialGradient> <radialgradient cx="119.095644%" cy="52.0335608%" fx="119.095644%" fy="52.0335608%" r="14.6438989%" id="radialGradient-14"> <stop stop-color="#494949" offset="0%"></stop> <stop stop-color="#161616" offset="100%"></stop> </radialGradient> <radialgradient cx="47.8895118%" cy="55.8923302%" fx="47.8895118%" fy="55.8923302%" r="3.51813166%" id="radialGradient-15"> <stop stop-color="#494949" offset="0%"></stop> <stop stop-color="#161616" offset="100%"></stop> </radialGradient> <radialgradient cx="52.085624%" cy="55.8923302%" fx="52.085624%" fy="55.8923302%" r="3.51794244%" id="radialGradient-16"> <stop stop-color="#494949" offset="0%"></stop> <stop stop-color="#161616" offset="100%"></stop> </radialGradient> <lineargradient x1="99%" y1="54%" x2="0%" y2="53%" id="linearGradient-17"> <stop stop-color="#555555" offset="49%"></stop> <stop stop-color="#000000" offset="100%"></stop> </linearGradient> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="wheel"> <path d="M355.884216,299.095764 C347.228517,296.223823 337.327685,301.635517 328.268951,299.095764 C304.945707,292.55674 278.921547,287.576843 261.5,287.576843 C243.927092,287.576843 220.602013,292.469854 197.072193,299.095778 C189.450269,301.242088 180.15386,296.6932 172.677521,299.095764 C131.760179,312.244784 97.5,334.5 97.5,334.5 L114.5,255.5 L114.5,136.5 L142.5,119.5 L136.5,56.5 L63.5,56.5 L34.5,45.5 L74.5,5.5 L136.5,0.5 L258.426346,0.5 L386.5,0.5 L448.5,5.5 L488.5,45.5 L448.5,56.5 L386.5,56.5 L380.5,119.5 L408.5,136.5 L408.5,255.5 L425.5,334.5 C425.5,334.5 394.555266,311.926728 355.884216,299.095764 Z" id="Shape" fill="#323232"></path> <path d="M404.33,289.75 C401.05,287.8 398.02,297.53 397.56,311.47 C397.1,325.42 399.39,338.31 402.67,340.25 C402.95,340.42 403.22,340.5 403.5,340.5" id="Shape" fill="url(#radialGradient-1)"></path> <polygon id="Shape" fill="#191919" points="391.5 39.5 335.5 68.5 278.097336 229.422699 289.5 260.5 233.5 260.5 245.987854 229.422699 187.5 68.5 131.5 39.5 131.5 5.5 136.5 0.5 386.5 0.5 391.5 5.5"></polygon> <g id="Group" transform="translate(397.000000, 28.000000)"> <polygon id="Shape" fill="url(#linearGradient-2)" points="125.5 51.5 125.5 249.5 62.5 312.5 11.5 312.5 11.5 261.5 51.5 261.5 79.5 232.5 79.5 176.5 57.5 153.5 11.5 153.5 11.5 96.5 62.5 96.5 79.5 79.5 79.5 57.5 51.5 28.5 79.5 0.5"></polygon> <polygon id="Shape" fill="url(#linearGradient-3)" points="53.34 153.5 0.5 153.5 0.5 96.5 59.08 96.5 85.5 108.5 85.5 142.5"></polygon> <polygon id="Shape" fill="url(#linearGradient-4)" points="6.5 312.5 6.5 261.5 52.97 261.5 62.5 312.5"></polygon> <polygon id="Shape" fill="url(#linearGradient-5)" points="62.5 312.5 51.5 261.5 79.5 232.5 125.5 249.5"></polygon> <polygon id="Shape" fill="url(#linearGradient-6)" points="125.5 51.5 79.5 57.5 51.5 28.5 79.5 0.5"></polygon> </g> <path d="M118.5,289.75 C121.78,287.8 124.81,297.53 125.27,311.47 C125.73,325.42 123.44,338.31 120.16,340.25 C119.89,340.42 119.61,340.5 119.33,340.5" id="Shape" fill="url(#radialGradient-7)"></path> <polygon id="Shape" fill="url(#linearGradient-8)" points="0.5 79.5 0.5 277.5 63.5 340.5 114.5 340.5 114.5 289.5 74.5 289.5 46.5 260.5 46.5 204.5 68.5 181.5 114.5 181.5 114.5 124.5 63.5 124.5 46.5 107.5 46.5 85.5 74.5 56.5 46.5 28.5"></polygon> <polygon id="Shape" fill="url(#linearGradient-9)" points="72.66 181.5 125.5 181.5 125.5 124.5 66.92 124.5 40.5 136.5 40.5 170.5"></polygon> <polygon id="Shape" fill="url(#linearGradient-10)" points="119.5 340.5 119.5 289.5 73.03 289.5 63.5 340.5"></polygon> <polygon id="Shape" fill="url(#linearGradient-11)" points="63.5 340.5 74.5 289.5 46.5 260.5 0.5 277.5"></polygon> <polygon id="Shape" fill="url(#linearGradient-12)" points="0.5 79.5 46.5 85.5 74.5 56.5 46.5 28.5"></polygon> <rect id="Rectangle-path" stroke="#000000" stroke-width="2.12" fill="#000000" x="221.5" y="157.5" width="233" height="22" rx="4"></rect> <circle fill="#656565" cx="357.5" cy="96.5" r="22.5"></circle> <circle fill="#4C4C4C" cx="359.5" cy="99.5" r="17.5"></circle> <circle stroke="#000000" stroke-width="2" fill="#000000" cx="356.5" cy="95.5" r="17.5"></circle> <text font-family="sans-serif" font-size="10" font-weight="normal" fill="#F7F4F8"> <tspan class="startbuttonlabel" x="318" y="133">{opts.startbuttonlabel}</tspan> </text> <circle id="Oval" fill="#656565" cx="158.5" cy="154.5" r="22.5"></circle> <circle id="Oval" fill="#4C4C4C" cx="160.5" cy="154.5" r="17.5"></circle> <circle id="Oval" stroke="#000000" stroke-width="2" fill="#000000" cx="156.5" cy="151.5" r="17.5"></circle> <circle id="Oval" fill="#656565" cx="159.5" cy="208.5" r="22.5"></circle> <circle id="Oval" fill="#4C4C4C" cx="161.5" cy="206.5" r="17.5"></circle> <circle id="Oval" stroke="#000000" stroke-width="2" fill="#000000" cx="157.5" cy="205.5" r="17.5"></circle> <rect id="Rectangle-path" stroke="#000000" stroke-width="1.12" fill="#000000" x="221.5" y="214.5" width="176" height="22"></rect> <circle id="Oval" stroke="#000000" stroke-width="4" fill="#323232" cx="262" cy="203" r="74"></circle> <rect id="Rectangle-path" stroke="#000000" stroke-width="2" fill="#656565" x="125.5" y="11.5" width="6" height="23"></rect> <rect id="Rectangle-path" stroke="#000000" stroke-width="2" fill="#656565" x="391.5" y="11.5" width="6" height="23"></rect> <circle id="Oval" stroke="#4A4A4A" stroke-width="4" fill="#6C696C" cx="262" cy="198" r="40"></circle> <path d="M124.67,124.77 C127.95,122.6 130.98,133.47 131.44,149.06 C131.9,164.65 129.61,179.05 126.33,181.23 C126.06,181.41 125.78,181.5 125.5,181.5" id="Shape" fill="url(#radialGradient-13)"></path> <path d="M398.33,124.78 C395.05,122.6 392.02,133.47 391.56,149.06 C391.1,164.65 393.39,179.05 396.67,181.23 C396.95,181.41 397.22,181.5 397.5,181.5" id="Shape" fill="url(#radialGradient-14)"></path> <path d="M46.29,28.73 C46.97,26.13 53.87,30.16 61.7,37.74 C69.54,45.31 75.34,53.56 74.66,56.16 C74.6,56.38 74.5,56.55 74.35,56.68" id="Shape" fill="url(#radialGradient-15)"></path> <path d="M476.59,28.48 C475.9,25.88 469,29.91 461.17,37.49 C453.33,45.06 447.53,53.31 448.22,55.91 C448.27,56.13 448.38,56.3 448.52,56.43" id="Shape" fill="url(#radialGradient-16)"></path> <text font-family="sans-serif" font-size="30" font-weight="normal" fill="#9B9B9B"> <tspan text-anchor="middle" class="wheellabel" x="260" y="47">{opts.wheellabel}</tspan> </text> <text font-family="sans-serif" font-size="10" font-weight="normal" fill="#F7F4F8"> <tspan class="primarybuttonlabel" x="162" y="126">{opts.primarybuttonlabel}</tspan> </text> <text font-family="sans-serif" font-size="10" font-weight="normal" fill="#F7F4F8"> <tspan class="secondarybuttonlabel" x="163" y="248">{opts.secondarybuttonlabel}</tspan> </text> </g> <g class="startbutton"> <circle id="startbuttonlightgo" stroke="#000000" stroke-width="2" fill="#51E413" cx="356.5" cy="95.5" r="12.5"></circle> <circle id="startbuttonlightstop" stroke="#000000" stroke-width="2" fill="#E70D0D" cx="356.5" cy="96.5" r="12.5"></circle> </g> <g id="forwarddrivebutton" transform="translate(377.000000, 134.000000)"> <rect id="Rectangle-path" fill="url(#linearGradient-17)" x="0.5" y="0.5" width="79" height="56" rx="6.59"></rect> <polygon id="Triangle" fill="#51E513" points="45 13 60 43 30 43"></polygon> </g> <g id="reversedrivebutton" transform="translate(377.000000, 205.000000)"> <rect id="Rectangle-path-Copy" fill="url(#linearGradient-17)" x="0" y="0" width="79" height="56" rx="6.59"></rect> <polygon id="Triangle" fill="#51E513" transform="translate(44.000000, 30.000000) scale(1, -1) translate(-44.000000, -30.000000) " points="59 45 29 45 44 15"></polygon> </g> <circle id="primarybutton" stroke="#000000" stroke-width="2" fill="#4990E2" cx="156.5" cy="151.5" r="12.5"></circle> <circle id="secondarybutton" stroke="#000000" stroke-width="2" fill="#F6A623" cx="157.5" cy="205.5" r="12.5"></circle> </g> </svg> </div>', '', '', function(opts) {
+
+	this.wheellabel = opts.wheellabel
+	this.primarybuttonlabel = opts.primarynuttonlabel
+	this.secondarybuttonlabel = opts.secondarybuttonlabel
+	this.startbuttonlabel = opts.startbuttonlabel
+
+	this.state = {}
+
+	this.sensitivity = 10
+
+	this.state.running = false
+	var self = this
+
+	this.on('mount', function(){
+
+		window.addEventListener("deviceorientation", getOrientation, true);
+
+		primaryButton = self.root.querySelector('#primarybutton')
+		secondaryButton = self.root.querySelector('#secondarybutton')
+
+		startButton = self.root.querySelector('.startbutton')
+		startButtonLightGo = self.root.querySelector('#startbuttonlightgo')
+		startButtonLightStop = self.root.querySelector('#startbuttonlightstop')
+
+		forwardDriveButton = self.root.querySelector('#forwarddrivebutton')
+		reverseDriveButton = self.root.querySelector('#reversedrivebutton')
+
+		startButton.addEventListener('touchstart', function(){
+			event.preventDefault()
+			startButtonPush()
+		})
+		startButton.addEventListener('mousedown', function(){
+			event.preventDefault()
+			startButtonPush()
+		})
+
+		primaryButton.addEventListener('touchstart', function(){
+			event.preventDefault()
+			primaryButtonPush()
+		})
+		primaryButton.addEventListener('mousedown', function(){
+			event.preventDefault()
+			primaryButtonPush()
+		})
+
+		secondaryButton.addEventListener('touchstart', function(){
+			event.preventDefault()
+			secondaryButtonPush()
+		})
+		secondaryButton.addEventListener('mousedown', function(){
+			event.preventDefault()
+			secondaryButtonPush()
+		})
+
+		forwardDriveButton.addEventListener('touchstart', function(){
+			event.preventDefault()
+			forwardDrive()
+		})
+		forwardDriveButton.addEventListener('mousedown', function(){
+			event.preventDefault()
+			forwardDrive()
+		})
+
+		forwardDriveButton.addEventListener('touchend', function(){
+			event.preventDefault()
+			stop()
+		})
+		forwardDriveButton.addEventListener('mouseup', function(){
+			event.preventDefault()
+			stop()
+		})
+
+		reverseDriveButton.addEventListener('touchstart', function(){
+			event.preventDefault()
+			reverseDrive()
+		})
+		reverseDriveButton.addEventListener('mousedown', function(){
+			event.preventDefault()
+			reverseDrive()
+		})
+
+		reverseDriveButton.addEventListener('touchend', function(){
+			event.preventDefault()
+			stop()
+		})
+		reverseDriveButton.addEventListener('mouseup', function(){
+			event.preventDefault()
+			stop()
+		})
+
+		this.on('update', function(){
+			this.trigger('change', self.state)
+		})
+	})
+
+	function getOrientation(event) {
+
+		if(event.beta) {
+			self.state.beta = event.beta
+		}
+	}
+
+	function getSteering() {
+		if(self.state.beta < -self.sensitivity) {
+
+			self.state.steer = "left"
+		} else if(self.state.beta > self.sensitivity) {
+
+			self.state.steer = "right"
+		} else {
+
+			self.state.steer = "neutral"
+		}
+		self.trigger('steer', self.state)
+	}
+
+	function startButtonPush() {
+		self.state.running = !self.state.running
+		if(self.state.running == true){
+			startButtonLightStop.style.display = "none";
+			self.state.ignition = "on"
+
+			this.interval = setInterval(getSteering, 500)
+		} else {
+			startButtonLightStop.style.display = "inline";
+			self.state.ignition = "off"
+			clearInterval(this.interval)
+		}
+		self.trigger('ignition', self.state)
+	}
+
+	function primaryButtonPush() {
+		self.trigger('primarybuttonpush', self.state)
+	}
+
+	function secondaryButtonPush() {
+		self.trigger('secondarybuttonpush', self.state)
+	}
+
+	function forwardDrive(){
+		if (self.state.running) {
+			self.state.drive = "forward"
+			self.trigger('drive', self.state)
+		}
+	}
+
+	function reverseDrive(){
+		if (self.state.running) {
+			self.state.drive = "reverse"
+			self.trigger('drive', self.state)
+		}
+	}
+
+	function stop(){
+		if (self.state.running) {
+			self.state.drive = "stop"
+			self.trigger('drive', self.state)
+		}
+	}
+
+});
+
 /* End Riot tags */
 
 /* Start iotUI.js */
